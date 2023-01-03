e1b8cc89b94e2bb49a4de9c9f4c7f3e385b96e0a
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-01-03 20:13:23 +0500

    metaverse ts project05  word counter app

---
+++ index.js
@@ -0,0 +1,64 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import { titleTimer } from "./src/_AppTitle.js";
+// counting no of words in a paragraph
+class _wordCounter {
+    constructor() {
+        this.Run();
+    }
+    // runing the whole app
+    async Run() {
+        await this.AppTitle();
+        await this.Main();
+    }
+    // autor watermark on app at the begening
+    async AppTitle() {
+        const title = chalkAnimation.neon(`__________________Welcome to M.B Word Counter App__________________`);
+        await titleTimer();
+        title.stop();
+        console.log(chalk.bgRed.italic(`                                                             Autor:"M.B"`));
+        return;
+    }
+    async Main() {
+        await this.UserInput();
+        // continue or exit
+        const continueORexit = await inquirer.prompt([{
+                type: "confirm",
+                name: "runORexit",
+                message: "Press Y to Countinue OR any key to exit"
+            }]);
+        if (continueORexit.runORexit === true) {
+            await this.Main();
+        }
+        else {
+            console.log(chalk.bgCyan("                    Thanks for Using M.B Word Counter App                          "));
+        }
+    }
+    async UserInput() {
+        // getting user input
+        const userInput = await inquirer.prompt([{
+                type: "input",
+                name: "inputWord",
+                validate: function inputNotEmpty(input) {
+                    if (input.length === 0) {
+                        return "Please Enter some text to continue";
+                    }
+                    return true;
+                }
+            }]);
+        let paragraph = userInput.inputWord;
+        // converting string to array and counting the length of true returned by fillter
+        function countWords(paragraph) {
+            return paragraph.split(' ').filter(function (num) {
+                return num != '';
+            }).length;
+        }
+        //displaying word count in paragraph  
+        if (paragraph.length !== 0) {
+            console.log(chalk.green(`Total Words Count: ${countWords(paragraph)}`));
+        }
+    }
+}
+let run = new _wordCounter();
